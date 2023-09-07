e620d6965a167e8be54159940baa461027cc309a
Om Tekade <omtekade914@gmail.com>
2023-09-07 17:48:05 +0530

    Socket integration with project is completed

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -12,7 +12,10 @@ import{
     FormItem
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Plus, Smile, SmileIcon } from "lucide-react";
+import { Plus } from "lucide-react";
+import { useModal } from "@/hooks/use-model-store";
+import { EmojiPicker } from "../emoji-picker";
+import { useRouter } from "next/navigation";
 
 interface ChatInputProps{
     apiUrl: string;
@@ -32,6 +35,9 @@ export const ChatInput = ({
     type
 } : ChatInputProps ) => {
 
+    const {onOpen} = useModal();
+    const router = useRouter();
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -49,6 +55,9 @@ export const ChatInput = ({
             });
 
             await axios.post(url, values);
+
+            form.reset();
+            router.refresh();
         }   
         catch(error){
             console.log(error);
@@ -69,7 +78,7 @@ export const ChatInput = ({
                                 <div className="relative p-4 pb-6">
                                     <button
                                         type="button"
-                                        onClick={() => {}}
+                                        onClick={() => onOpen("messageFile", {apiUrl, query})}
                                         className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
                                     >
                                         <Plus 
@@ -82,8 +91,12 @@ export const ChatInput = ({
                                         placeholder={`Message ${type === "conversation" ? name : "#" + name}`}
                                         {...field}
                                     />
-                                    <div className="absolute top-7 right-8">
-                                        <Smile />
+                                    <div 
+                                        className="absolute top-7 right-8"
+                                    >
+                                        <EmojiPicker 
+                                            onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)}
+                                        />
                                     </div>
                                 </div>
                             </FormControl>
--- components/providers/model-provider.tsx
+++ components/providers/model-provider.tsx
@@ -10,6 +10,8 @@ import { LeaveServerModal } from "../models/leave-server-modal";
 import { DeleteServerModal } from "../models/delete-server-modal";
 import { DeleteChannelModal } from "../models/delete-channel-modal";
 import { EditChannelModal } from "../models/edit-channel-modal";
+import { MessageFileModal } from "../models/message-file-modal";
+import { DeleteMessageModal } from "../models/delete-message-modal";
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -33,6 +35,8 @@ export const ModalProvider = () => {
             <DeleteServerModal />
             <DeleteChannelModal />
             <EditChannelModal />
+            <MessageFileModal />
+            <DeleteMessageModal />
         </>
     )
 }
